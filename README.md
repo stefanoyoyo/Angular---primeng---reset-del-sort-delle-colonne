a39a9ca29ee917c70c61c0cc65fa85823cbcc14d
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2023-01-25 14:34:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular---primeng---reset-del-sort-delle-colonne
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-tablesort-demo-vwlrnx)
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent }   from './app.component';
+import { ProductService } from './productservice';
+
+import {TableModule} from 'primeng/table';
+import {ButtonModule} from 'primeng/button';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    TableModule,
+    ButtonModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ProductService]
+})
+
+export class AppModule { }
